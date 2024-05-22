9fba6a6273375965cb43c949080d93d38befeff4
lancasterjoe <168177546+lancasterjoe@users.noreply.github.com>
2024-05-22 15:54:38 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 300) {
+        let C = ""
+        reel = randint(1, 9)
+        music.play(music.stringPlayable("C D E F G A B C5 ", 120), music.PlaybackMode.InBackground)
+        basic.pause(500)
+        if (reel == 1) {
+            basic.showIcon(IconNames.Happy)
+            radio.sendValue(C, 1)
+        }
+        if (reel == 2) {
+            basic.showIcon(IconNames.Sad)
+            radio.sendValue(C, 2)
+        }
+        if (reel == 3) {
+            basic.showIcon(IconNames.Confused)
+            radio.sendValue(C, 3)
+        }
+        if (reel == 4) {
+            basic.showIcon(IconNames.Angry)
+            radio.sendValue(C, 4)
+        }
+        if (reel == 5) {
+            basic.showIcon(IconNames.Asleep)
+            radio.sendValue(C, 5)
+        }
+        if (reel == 6) {
+            basic.showIcon(IconNames.Surprised)
+            radio.sendValue(C, 6)
+        }
+        if (reel == 7) {
+            basic.showIcon(IconNames.Silly)
+            radio.sendValue(C, 7)
+        }
+        if (reel == 8) {
+            basic.showIcon(IconNames.Fabulous)
+            radio.sendValue(C, 8)
+        }
+        if (reel == 9) {
+            basic.showIcon(IconNames.Meh)
+            radio.sendValue(C, 9)
+        }
+    }
+})
+let reel = 0
+basic.showLeds(`
+    # # # . .
+    . . . # .
+    . # # . .
+    . . . # .
+    # # # . .
+    `)
+radio.setGroup(1)
